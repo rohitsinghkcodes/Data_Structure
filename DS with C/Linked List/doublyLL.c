28cd4ff98c6126ecbfde58dbb7a7224a818778d9
Rohit Singh <conankmails@gmail.com>
2021-07-07 21:59:08 +0530

    insertion in doubly linked list done

--- DS with C/Linked List/doublyLL.c
+++ DS with C/Linked List/doublyLL.c
@@ -7,9 +7,135 @@ struct DllNode{
     struct DllNode * next;
 };
 
+struct DllNode * insertBegin(struct DllNode *head, int data)
+{
+    struct DllNode * curr = head;
+    struct DllNode * new = (struct DllNode * ) malloc(sizeof(struct DllNode));
+    if(!new)
+    {
+        printf("\nMemory Error!");
+        return head;
+    }
+    new->prev = NULL;
+    new->data = data;
+    new->next = NULL;
+
+    if(curr == NULL)        //no previous node
+    {
+        head = new;
+        return head;
+    }
+    
+    //already some node exits
+    new->next = head;
+    head->prev = new;
+    head = new;
+    return head;
+}
+
+struct DllNode * insertEnd(struct DllNode * head, int data)
+{
+    struct DllNode * new = (struct DllNode *) malloc(sizeof(struct DllNode));
+     struct DllNode * curr = head;
+    if(!new)
+    {
+        printf("\nMemory Error!");
+        return head;
+    }
+    new->prev = NULL;
+    new->data = data;
+    new->next = NULL;
+
+    if(head == NULL)
+    {
+        head = new;
+        return head;
+    }
+
+    while(curr->next!=NULL)
+    {
+        curr=curr->next;
+    }
+    curr->next = new;
+    new->prev = curr;
+    return head;
+
+
+}
+
+struct DllNode * insertAtPosition(struct DllNode * head, int data, int pos)
+{
+    struct DllNode *new = (struct DllNode *) malloc (sizeof(struct DllNode ));
+    struct DllNode *temp = head;
+    //pos--;
+    if(!new)
+    {
+        printf("\nMemory Error!");
+        return head;
+    }
+
+    new->prev = NULL;
+    new->data = data;
+    new->next = NULL;
+
+    if(pos<=1)
+    {
+        new->next = head;
+       if(head)
+       {
+            head->prev = new;
+       }
+        head = new;
+        return head;
+    }
+
+    int k = 1;
+    while(k<pos-1 && temp->next!=NULL)
+    {
+        temp=temp->next;
+        k++;
+    }
+    if(k<pos-1)
+    {
+        printf("\n positon not available!");
+        return head;
+    }
+
+    new->next = temp->next;
+    new->prev = temp;
+    if(temp->next)
+    {
+        temp->next->prev = new;
+    }
+    temp->next =new;
+    return head;
+
+    // Another method
+    // while(pos-- && temp!=NULL)
+    // {
+    //     temp=temp->next;
+    // }
+    // if(temp == NULL)
+    // {
+    //     printf("\n positon not available!");
+    //     return head;
+    // }
+    // else{
+    //     new->next = temp->next;  //if last node it will store null
+    // new->prev = temp;
+    // if(temp->next)
+    // {
+    //     temp->next->prev = new;
+    // }
+    // temp->next = new;
+    // }
+    // return head;
+}
+
 //for traversing in the linked list
 void llTraversal(struct DllNode *head)
 {
+    printf("\n*****************************************************\n\n");
     if (head == NULL)
     {
         printf("\n SORRY!! NO NODES. PLEASE INSERT SOME.");
@@ -24,20 +150,55 @@ void llTraversal(struct DllNode *head)
 
 int main()
 {
-    struct DllNode * head,* second, *third;
-    head = (struct DllNode *)malloc (sizeof(struct DllNode));
-    second = (struct DllNode *)malloc (sizeof(struct DllNode));
-    third = (struct DllNode *)malloc (sizeof(struct DllNode));
-    head->prev = NULL;
-    head->data = 10;
-    head->next = second;
-    second->prev = head;
-    second->data = 20;
-    second->next = third;
-    third->prev = second;
-    third->data = 30;
-    third->next = NULL;
+    struct DllNode * head=NULL;
+   int d, p;
+    while (1)
+    {
+        int ch;
 
-    llTraversal(head);
-
+        printf("\n\n*****************************************************\nSelect \n \t 1 : Inserting node in the beginning.\n \t 2 : Inserting node at the end.\n \t 3 : Inserting node at a perticular position.\n \t 4 : Deleting node in the beginning.\n \t 5 : Deleting node at the end.\n \t 6 : Deleting node at a perticular position.\n \t 7 : Displaying Linked List elements.\n \t 8 : Exit \n Enter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\n Enter the data to be stored in beginning: ");
+            scanf("%d", &d);
+            head = insertBegin(head,d);
+            break;
+        case 2:
+            printf("\n Enter the data to be stored at end: ");
+            scanf("%d", &d);
+             head = insertEnd(head,d);
+            break;
+        case 3:
+            printf("\n Enter the position and data: ");
+            scanf("%d %d", &p, &d);
+            head = insertAtPosition(head, d, p);
+            break;
+        case 4:
+            printf("\n Deleting from the front ");
+            //head = deleteFront(head);
+            break;
+        case 5:
+            printf("\n Deleting from last element ");
+            //head = deleteLast(head);
+            break;
+        case 6:
+            printf("\n Deleting element at positon x ");
+            printf("\n Enter the position to delete node: ");
+            scanf("%d", &p);
+            //head = deletePosition(head,p);
+            break;
+        case 7:
+            printf("\n The linked list elements are: \n ");
+            llTraversal(head);
+            break;
+        case 8:
+            exit(0);
+            break;
+        default:
+            printf("\nWrong choice!");
+            break;
+        }
+    }
 }
